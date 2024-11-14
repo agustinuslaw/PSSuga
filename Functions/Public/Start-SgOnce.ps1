ad49e555bd1c9b2ee6cf1c934295208f921fe3f8
Agustinus Lawandy <agustinus.lawandy@systema.com>
2024-11-15 00:05:36 +0100

    Start-SgOnce better arg

--- Functions/Public/Start-SgOnce.ps1
+++ Functions/Public/Start-SgOnce.ps1
@@ -1,31 +1,33 @@
 Function Start-SgOnce {
-    Param(
-		[Parameter(Position=0)][String]$FilePath
-		,[String]$Filter
-		,[String]$WorkDir
-		,[String[]]$Arg
-		,[switch]$Admin
-		,[String]$LogPath
+	Param(
+		[Parameter(Position = 0)][String]$FilePath
+		, [String]$Filter
+		, [String]$WorkingDirectory
+		, [String[]]$ArgumentList
+		, [switch]$Admin
+		, [String]$LogPath
 	)
 
-	$Logger=New-SgLogger -Path $LogPath
+	$Logger = New-SgLogger -Path $LogPath
 	
-	$Filter=Get-SgStringOrDefault $Filter "Name='$FilePath'"
-	$WorkDir=Get-SgStringOrDefault $WorkDir "."
+	$Filter = Get-SgStringOrDefault $Filter "Name='$FilePath'"
+	$WorkingDirectory = Get-SgStringOrDefault $WorkingDirectory "."
 		
 	$Logger.log("Start once: $FilePath")
 	
 	# Look for matching process 
-	$Process=Get-CimInstance Win32_Process -Filter $Filter
-	if(!$Process) {
+	$Process = Get-CimInstance Win32_Process -Filter $Filter
+	if (!$Process) {
 		# No matching process, start
-		if ($AsAdmin){
+		if ($AsAdmin) {
 			$Logger.log("With admin rights")
-			Start-Process $FilePath -Work $WorkDir -ArgumentList $Arg -Verb RunAs *>&1 | tee -encoding utf8 -append -filepath $LogPath -ErrorAction Ignore
-		} else {
-			Start-Process $FilePath -Work $WorkDir -ArgumentList $Arg *>&1 | tee -encoding utf8 -append -filepath $LogPath -ErrorAction Ignore
+			Start-Process $FilePath -Work $WorkingDirectory -ArgumentList $ArgumentList -Verb RunAs *>&1 | tee -encoding utf8 -append -filepath $LogPath -ErrorAction Ignore
 		}
-	} else {
+		else {
+			Start-Process $FilePath -Work $WorkingDirectory -ArgumentList $ArgumentList *>&1 | tee -encoding utf8 -append -filepath $LogPath -ErrorAction Ignore
+		}
+	}
+ else {
 		# Do not start if process already running
 		$Logger.log("Process already started for filter: $Filter")
 	}
